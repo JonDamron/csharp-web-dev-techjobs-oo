22aff26e3aa4b47efd7d1d9da3b8ca65accc5dd1
Jon Damron <Damron.Jon.G@gmail.com>
2021-02-11 21:10:13 -0600

    Generated Gethash and Equals in Job class

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -28,6 +28,19 @@ namespace TechJobsOO
             JobType = jobType;
             JobCoreCompetency = jobCoreCompetency;
         }
+
         // TODO: Generate Equals() and GetHashCode() methods.
+
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        
     }
 }
