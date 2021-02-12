8dba849388acadb92a70efedc5a61cee8ba53d12
Jon Damron <Damron.Jon.G@gmail.com>
2021-02-11 20:25:45 -0600

    Classes complete

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Job testjob1;
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
